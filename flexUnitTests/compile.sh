9803278bb0836d52987aa7c4a0700f97511cabc7
amacdougall <smoke@alanmacdougall.com>
2012-09-03 15:01:13 -0400

    Update compile.sh to omit full mxmlc path.

--- flexUnitTests/compile.sh
+++ flexUnitTests/compile.sh
@@ -1,7 +1,11 @@
-/usr/local/flex_sdk_4.5.1.21328/bin/mxmlc \
+mxmlc
     -static-link-runtime-shared-libraries=true \
 	-debug=true \
 	-output=build/test.swf \
 	-library-path+=./libs \
 	-source-path+=./src \
 	src/TestContainer.mxml
+
+" my personal mxmlc location -- @amacdougall
+" /usr/local/flex_sdk_4.5.1.21328/bin/mxmlc \
+"
